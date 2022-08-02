2ac45ca6f2a1d350cbcfde753d4d0a29a8fb97a0
kat mac <theyounglegend59@gmail.com>
2022-08-02 11:08:30 +0200

    Added character class

---
+++ Gade 1B part 1/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gade_1B_part_1
+{
+    internal class Character: Tile
+    {
+        protected int hp;
+        protected int maxHp;
+        protected int damage;
+
+        public int HP { get { return hp; } set { hp = value; } }
+        public int MaxHp { get { return maxHp; } set { maxHp = value; } }
+        protected int Damage { get { return damage; } set { damage = value; } }
+
+
+        public Character(int x, int y) : base (x,y)
+        {
+            
+        }
+
+    }
+}
