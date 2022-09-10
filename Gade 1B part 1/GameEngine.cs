37a3c03f5c96c11996a20eebadd5e39189ad10c4
kat mac <theyounglegend59@gmail.com>
2022-09-10 18:44:12 +0200

    Added movement for player in Game Engine Class

--- Gade 1B part 1/GameEngine.cs
+++ Gade 1B part 1/GameEngine.cs
@@ -10,13 +10,14 @@ namespace Gade_1B_part_1
     {
         private Map? map;
        
-        private static char hero = (char)208;
+        private static Hero? Hero = new Hero(5, 5, 20, 20, 2, HeroChar); //fix char
+        private static char heroChar = (char)208;
         private static char empty = (char)44;
         private static char swampCreature = (char)199;
         private static char obstacle = (char)42;
 
         public Map? Map { get { return map; } set { map = value; } }
-        public char Hero { get { return hero; }}
+        public static char HeroChar { get { return heroChar; }}
         public char Empty { get { return empty; }}
         public char SwampCreature { get { return swampCreature; }}
         public char Obstacle { get { return obstacle; }}
@@ -29,26 +30,40 @@ namespace Gade_1B_part_1
             {
                 if (direction == Character.MovementEnum.Up)
                 {
+                    Hero!.X = Hero.X + 1;
+
+
                     return true;
                 }
 
                 if (direction == Character.MovementEnum.Down)
                 {
+                    Hero!.X = Hero.X - 1;
+
                     return true;
                 }
 
                 if (direction == Character.MovementEnum.Left)
                 {
+                    Hero!.Y = Hero.Y - 1;   
+
                     return true;
                 }
 
                 if (direction == Character.MovementEnum.Right)
                 {
+                    Hero!.X = Hero.X + 1;
+
                     return true;
                 }
 
                 else if (direction == Character.MovementEnum.NoMovement)
                 {
+                    Hero!.X = Hero.X + 0;
+                    Hero!.X = Hero.X - 0;
+                    Hero!.Y = Hero.Y + 0;
+                    Hero!.Y = Hero.Y - 0;
+
                     return false;
                 }
                 return false;
@@ -61,7 +76,7 @@ namespace Gade_1B_part_1
 
             
 
-            return base.ToString();
+            return base.ToString()!;
           }
 
 
